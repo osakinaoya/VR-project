cb3f861783e558b98edf114a99035644cebb558d
osakinaoya <nao1023.oz@gmail.com>
2020-11-11 10:13:23 +0900

    inputシステムと手追加

---
+++ VR-project/Assets/Scripts/OVRControllerinput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVRControllerinput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
+        {
+            Debug.Log("右人差し指トリガーを押したよ！");
+        }
+    }
+}
